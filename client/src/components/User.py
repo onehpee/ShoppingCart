ce01dfeec10ba6ed24b15a966e22ab1b61f5449e
Adedayo UWensuyi <onehpee@yahoo.com>
2023-11-02 22:57:39 -0500

    Setting up Enviroment

---
+++ client/src/components/User.py
@@ -0,0 +1,6 @@
+class User:
+    def __init__(self, firstName, lastName, userName, password,):
+        self.firstName = firstName
+        self.lastName = lastName
+        self.userName = userName
+        self.password = password
