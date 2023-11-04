5e050c219838e4f635808fff21b91cd8718b148b
Adedayo UWensuyi <onehpee@yahoo.com>
2023-11-03 22:54:53 -0500

    getters and setters setup

---
+++ client/src/components/Main.py
@@ -0,0 +1,5 @@
+from User import User
+
+user = User("Adedayo","Uwensuyi","Mysterio619",619)
+
+user.get_firstName()
--- client/src/components/User.py
+++ client/src/components/User.py
@@ -4,3 +4,28 @@ class User:
         self.lastName = lastName
         self.userName = userName
         self.password = password
+
+
+    def get_firstName(self):
+        return self.get_firstName
+    
+    def set_firstName(self, value):
+        self._firstName = value
+
+    def get_lastName(self):
+        return self.get_lastName
+    
+    def set_lastName(self, value):
+        self._lastName = value
+
+    def get_userName(self):
+        return self.get_userName
+    
+    def set_usertName(self, value):
+        self._userName = value
+
+    def get_password(self):
+        return self.get_password
+    
+    def set_password(self, value):
+        self._password = value
